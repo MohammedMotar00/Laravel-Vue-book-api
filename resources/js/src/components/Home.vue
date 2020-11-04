bbdd1f1194325857f9a7d2c274f44be3ecda0726
Mohammed Motar <mohamenmd.motar.mm@gmail.com>
2020-11-04 15:02:28 +0100

    Setup the project

---
+++ resources/js/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Home Component</h1>
+    <router-link to="/list">go to list</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: red;
+}
+</style>
